008b8876fc663728825f9faade56c1e964021368
Johannes Helleve <Johanshelleve@gmail.com>
2023-05-23 11:17:04 +0200

    trøbbel

--- OppskriftOgTilbud.py
+++ OppskriftOgTilbud.py
@@ -1,3 +1,22 @@
 import requests
+import config
 
-api_key=
+
+API_KEY = config.api_key
+
+# Make the request to the Tjek API
+response = requests.get("https://api.tjek.no/v1/products", headers={"Authorization": "Bearer " + API_KEY}, max_retries=5)
+
+if response.status_code == 200:
+    # Get the products from the response
+    products = response.json()["products"]
+
+    # Filter the products by store and sale
+    products = [product for product in products if product["brand"] == "Bunnpris" and product["sale"] == True]
+
+    # Print the list of products
+    for product in products:
+        print(product["name"] + " is on sale for " + product["price"])
+
+else:
+    print("Error: " + response.status_code)
