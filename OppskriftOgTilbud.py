9b8429c9bc8e9ba9739abad9110cdb2611d8b286
Johannes Helleve <Johanshelleve@gmail.com>
2023-06-03 19:12:44 +0200

    Prøve å formatere json

--- OppskriftOgTilbud.py
+++ OppskriftOgTilbud.py
@@ -1,10 +1,14 @@
 import requests
 import config
-import _json
+import json
 
 endpoint = 'https://kassal.app/api/v1/'
 headers = {"Authorization": "Bearer " + config.BearerToken}
 
 response = requests.get('https://kassal.app/api/v1/products', headers=headers)
-print(response)
-print(response.json())
+
+response_data = json.loads(response.json)
+
+response_formated = json.dumps(response_data, indent = 2 )
+
+print(response_formated)
