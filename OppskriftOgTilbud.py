e4848cdef7da338e34e75dea739da056754cb442
Johannes Helleve <Johanshelleve@gmail.com>
2023-06-03 10:45:09 +0200

    Fant en annen API, som er bedre

--- OppskriftOgTilbud.py
+++ OppskriftOgTilbud.py
@@ -1,22 +1,10 @@
 import requests
 import config
+import _json
 
+endpoint = 'https://kassal.app/api/v1/'
+headers = {"Authorization": "Bearer " + config.BearerToken}
 
-API_KEY = config.api_key
-
-# Make the request to the Tjek API
-response = requests.get("https://api.tjek.no/v1/products", headers={"Authorization": "Bearer " + API_KEY})
-
-if response.status_code == 200:
-    # Get the products from the response
-    products = response.json()["products"]
-
-    # Filter the products by store and sale
-    products = [product for product in products if product["brand"] == "Bunnpris" and product["sale"] == True]
-
-    # Print the list of products
-    for product in products:
-        print(product["name"] + " is on sale for " + product["price"])
-
-else:
-    print("Error: " + response.status_code)
+response = requests.get('https://kassal.app/api/v1/products', headers=headers)
+print(response)
+print(response.json())
