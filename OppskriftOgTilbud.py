9f3d8520e15372eeaf8649092a7dc60dc8fbd756
Johannes Helleve <Johanshelleve@gmail.com>
2023-06-06 23:26:51 +0200

    Kommunikasjon med MongoDB

--- OppskriftOgTilbud.py
+++ OppskriftOgTilbud.py
@@ -6,9 +6,23 @@ from pymongo import MongoClient
 headersKassal = {"Authorization": "Bearer " + config.BearerToken}
 headersMongo = {}
 
-client = MongoClient()
-db = client['test']
+test3 = [{'name': 'Audi', 'price': 52642},
+    {'name': 'Mercedes', 'price': 57127},
+    {'name': 'Skoda', 'price': 9000},
+    {'name': 'Volvo', 'price': 29000},
+    {'name': 'Bentley', 'price': 350000},
+    {'name': 'Citroen', 'price': 21000},
+    {'name': 'Hummer', 'price': 41400},
+    {'name': 'Volkswagen', 'price': 21600}
+    ]
+client = MongoClient(config.UserMongo)
+
+db = client.Kassal
+
+db.Test.insert_many(test3)
+
 response = requests.get('https://kassal.app/api/v1/products', headers=headersKassal)
+print(response)
 
+db.Kassal.insert_one(response)
 
-#print(response.json())
