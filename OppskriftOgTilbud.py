401e275251c163a3a89c6fb935a2995ee342ee88
Johannes Helleve <Johanshelleve@gmail.com>
2023-06-27 19:54:38 +0200

    ser ut som kassalapp apien er oppe og går

--- OppskriftOgTilbud.py
+++ OppskriftOgTilbud.py
@@ -17,7 +17,7 @@ client = MongoClient(MongoAPI)
 db = client.Kassal
 
 
-response = requests.get('https://kassal.app/api/v1/products/', headers=headersKassal)
+response = requests.get('https://kassal.app/api/v1/products/?search=fajita mix&size=60&sort=price_desc', headers=headersKassal)
 
 print(response)
 #@limits(calls=60, period=60)
@@ -27,6 +27,17 @@ def get_grocery_data():
         return data
     else:
         raise Exception('Error getting total items')
+
+"""responesFormated = json.dumps(get_grocery_data()['data'][0], indent=2)
+print(responesFormated)
+print(type(responesFormated))"""
+print(type(get_grocery_data()['data'][0]))
+i = 0
+print(len(get_grocery_data()['data']))
+while i < len(get_grocery_data()['data']):
+   print(f"{get_grocery_data()['data'][i]['name']} koster {get_grocery_data()['data'][i]['price_history'][0]['price']} kr idag {get_grocery_data()['data'][i]['store']['name']}")
+   i += 1
+
 
 #formating the data
 def alter_data_bson(data):
--- recipeAndPrice.py
+++ recipeAndPrice.py
@@ -1,15 +1,16 @@
 import json
-
 from oppskrifterOpen import make_recipe
-from OppskriftOgTilbud import push_to_mongo
+from OppskriftOgTilbud import push_to_mongo, get_grocery_data
 
-recipe = make_recipe()
+#recipe = make_recipe()
 
 
+"""
 message = recipe['choices'][0]['message']['content']
-print(message)
-message['totPirce'] = 23
-print(message)
+messageDict = json.loads(message)
+print(type(messageDict))
+messageDict['totPirce'] = 23
+print(messageDict)"""
 
 """try:
     with open("oppskirft.json", "w") as outfile:
@@ -17,8 +18,10 @@ print(message)
 except:
     print(recipe[4][1][1])"""
 
-"""f = open('oppskirft.json')
+f = open('oppskirft.json')
 
 data = json.load(f)
 
-print(data['choices'][0]['message']['content'])"""
+print(data['choices'][0]['message']['content'])
+print(type(data))
+print(get_grocery_data())
